70a274ab5cac6c180edc583fca9f66f2729eebf7
Jerry xu <jerryxu521@gmail.com>
2020-03-31 22:27:31 +0800

    gql

---
+++ src/query/card.ts
@@ -0,0 +1,17 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+
+  type Book {
+    title: String
+    author: String
+  }
+  
+  type Query {
+    books: [Book]
+  }
+
+
+
+`;
+export default typeDefs;
